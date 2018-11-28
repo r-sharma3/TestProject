fa705b4b0bd30b6d8f6406727e14adb10ec7bf54
Rahul Sharma <rahul.sharma@intsof.com>
2018-11-28 13:38:19 +0530

    Removed gitignore file

---
+++ Decorist/src/com/decorist/designer/Designer_Login.java
@@ -0,0 +1,25 @@
+package com.decorist.designer;
+/*package com.decorist.designer;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import com.decorist.businessrules.BusinessFunctions;
+import com.decorist.businessrules.Decorist_Constants;
+import com.org.core.Assertions;
+import com.org.core.Retry;
+
+@Listeners(com.org.core.Report.class)
+public class Designer_Login extends BusinessFunctions {
+	@Test(retryAnalyzer = Retry.class)
+	public void nuRentalDigitalGmStoreGC() {
+		try {
+			Assertions.initExtentReport(this.getClass().getSimpleName(), true);
+			envinfo();
+			Assertions.testStepMessage(Decorist_Constants.STEP_1 + "Launch Decorist URL");
+			Assertions.testStepMessage(Decorist_Constants.STEP_2 + "Login to the Application as Designer");
+			loginAsDesigner();
+		} catch (Exception e) {
+			logReport(false, Decorist_Constants.FAILED + e.getMessage(), true);
+		}
+	}
+}*/
